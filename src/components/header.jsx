61028f6fe3fd92876aa2bd1ca0a43bc28b0537b2
Guilherme <guilhermehenrike0412@gmail.com>
2026-09-18 22:19:02 -0300

    feat: header v2

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
 import "./header.css";
 
 function Header() {
+
+    const [menuAberto, setMenuAberto] = useState(false);
+
     return (
         <header className="header">
-            <h1>Music Hub</h1>
+
+            <button
+                className="botao-menu"
+                onClick={() => setMenuAberto(!menuAberto)}
+            >
+                <FiMenu />
+            </button>
+
+            <h1>YrPlace</h1>
+
+            {menuAberto && (
+                <div className="menu">
+                    <button>Editar Perfil</button>
+                    <button>Trocar Senha</button>
+                    <button>Sair</button>
+                </div>
+            )}
+
         </header>
     );
 }
